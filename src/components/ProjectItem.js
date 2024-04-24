645538b46fbbce9c48e7f4a0adb9c7d51126c512
student <kmac.kimaru93@gmail.com>
2024-04-24 23:19:08 +0300

    adding lists and keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(name)
+  console.log(about)
+  console.log(technologies)
+  const techLists = technologies.map((techList, index) => {
+  return <span key={index} >{techList}</span>
+  });
+
+  /* const techString = techList.join(", ") */
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+       {techLists}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+ console.log(projects);
+ 
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+     
+    <ProjectItem key = {projects.id} name = {projects[0].name} about={projects[0].about} technologies={projects[0].technologies} />
+    <ProjectItem key = {projects.id} name = {projects[1].name} about={projects[1].about} technologies={projects[1].technologies} />
+    <ProjectItem key = {projects.id} name = {projects[2].name} about={projects[2].about} technologies={projects[2].technologies} />
+
     </div>
   );
 }
